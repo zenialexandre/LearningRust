2987d679632e5cf0927014e01b1ce4ecd2779f8c
zenialexandre <zenialexandre123@gmail.com>
2023-06-21 21:43:40 -0300

    creating simple flow for choosing exercise

---
+++ src/leap_year.rs
@@ -0,0 +1,4 @@
+
+pub fn init_leap_year() {
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,38 @@
+use std::io;
+use std::io::Write;
 use reversing_string::init_reversing_string;
+use crate::leap_year::init_leap_year;
 
 mod reversing_string;
+mod leap_year;
 
 fn main() {
-    init_reversing_string();
+    choose_exercise();
+}
+
+fn choose_exercise() {
+    let mut exercise_input = String::new();
+    
+    print!("Please, choose a exercise: ");
+    io::stdout().flush().expect("Failed to flush.");
+    io::stdin().read_line(&mut exercise_input).expect("Failed to read line.");
+    search_inputted_exercise(exercise_input.trim().to_string());
+}
+
+fn search_inputted_exercise(exercise_input: String) {
+    let exercises_vector = vec!["reversing_string".to_string(), "leap_year".to_string()];
+
+    if exercises_vector.contains(&exercise_input) {
+        run_chosen_exercise(exercise_input, exercises_vector)
+    } else {
+        panic!("Exercise not found.");
+    }
+}
+
+fn run_chosen_exercise(exercise_input: String, exercises_vector: Vec<String>) {
+    if exercise_input.eq(&exercises_vector[0]) {
+        init_reversing_string();
+    } else if exercise_input.eq(&exercises_vector[1]) {
+        init_leap_year();
+    }
 }
